75b122a7ae706f9675efaccd4e67bbb96fc42628
Gee <alickoamarko@gmail.com>
2026-01-09 07:17:11 +0100

    Preallocate MAX_PLAYERS memory for containers

--- main.cpp
+++ main.cpp
@@ -156,9 +156,21 @@ std::vector<enet_uint8> player_ids = []{
 	v.reserve(MAX_PLAYERS);
 	return v;
 }();
-std::unordered_map<enet_uint8, PlayerState> player_states;
-std::unordered_map<enet_uint8, ServerPlayerData> serverside_player_data;
-std::unordered_map<enet_uint8, PlayerStats> player_stats;
+std::unordered_map<enet_uint8, PlayerState> player_states = []{
+	std::unordered_map<enet_uint8, PlayerState> m;
+	m.reserve(MAX_PLAYERS);
+	return m;
+}();
+std::unordered_map<enet_uint8, ServerPlayerData> serverside_player_data = []{
+	std::unordered_map<enet_uint8, ServerPlayerData> m;
+	m.reserve(MAX_PLAYERS);
+	return m;
+}();
+std::unordered_map<enet_uint8, PlayerStats> player_stats = []{
+	std::unordered_map<enet_uint8, PlayerStats> m;
+	m.reserve(MAX_PLAYERS);
+	return m;
+}();
 
 std::string map_data;
 Vec3 hider_spawn = {};
